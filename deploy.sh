db7d4b4292d7781381c72c537201a719772f0ebb
Junichi Takagi <junichi.takagi@ebisol.co.jp>
2018-01-22 12:26:08 +0900

    edit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker build -t junzzz/docker_copy_test/app:$CIRCLE_SHA1 .
-docker push junzzz/docker_copy_test/app:$CIRCLE_SHA1
+docker build -t junzzz/docker_copy_test:$CIRCLE_SHA1 .
+docker push junzzz/docker_copy_test:$CIRCLE_SHA1
